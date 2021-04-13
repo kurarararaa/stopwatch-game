27d5509f274bb5c629fc1964265845ee0f5bb0db
azukisiromochi <azukisiromochi@gmail.com>
2021-04-13 14:23:12 +0900

    ✨ クラッカーサンプルを追加

---
+++ CrackerBackground.vue
@@ -0,0 +1,12 @@
+<template>
+  <canvas class="cracker-background" v-cracker></canvas>
+</template>
+<style scoped>
+.cracker-background {
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+</style>
